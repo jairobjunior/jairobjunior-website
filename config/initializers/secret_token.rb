c8a1ad57139542604f38f3ccb13dbe69fd148d9a
jairobjunior <jairobjunior@gmail.com>
2012-09-10 11:48:54 -0700

    including firts files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jairbjunior::Application.config.secret_token = 'fb8ab6a690f1fe064d97bd09d3402258e2df8b87560b9b8f31e6055c7bb19413f99657f97ab48ab0a065337f7c30a6a0ff36dc6cd5bcf85e2f6ade7d26e6d3cc'
